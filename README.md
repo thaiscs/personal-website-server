a12988a2160ffeb3ea7dd5bad7c435873d081d0a
thaiscs <thais.advj@gmail.com>
2022-11-02 19:00:01 +0100

    Backend setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Website Server for Nodemailer
---
+++ config.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config();
+module.exports = {
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS,
+};
